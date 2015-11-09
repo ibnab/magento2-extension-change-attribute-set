40ed73b0903c98a87ffb57de138ece38d4b7ee38
said elmarrouani <contact@ibnab.com>
2015-11-09 00:46:38 +0000

    first commit extension

---
+++ README.md
@@ -0,0 +1,5 @@
+#Ibnab Magento 2 Extension Change Attribute Set
+1 Download
+2 past in your folder app/code
+3 enable extension in app/etc/config.php by adding 'Ibnab_DeleteOrder' => 1,
+4 and execute the command php bin/magento setup:upgrade
---
+++ Ui/Component/MassAction/Group/Options.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Ibnab\ChangeAttributeSet\Ui\Component\MassAction\Group;
+
+use Magento\Framework\UrlInterface;
+use Zend\Stdlib\JsonSerializable;
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\Set\CollectionFactory;
+use Magento\Catalog\Model\ResourceModel\Product;
+/**
+ * Class Options
+ */
+class Options implements JsonSerializable
+{
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+    /**
+     * @var CollectionFactory
+     */
+    protected $product;
+    /**
+     * Additional options params
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Base URL for subactions
+     *
+     * @var string
+     */
+    protected $urlPath;
+
+    /**
+     * Param name for subactions
+     *
+     * @var string
+     */
+    protected $paramName;
+
+    /**
+     * Additional params for subactions
+     *
+     * @var array
+     */
+    protected $additionalData = [];
+
+    /**
+     * Constructor
+     *
+     * @param CollectionFactory $collectionFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $data
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        UrlInterface $urlBuilder,
+        Product $product,
+        array $data = []
+    ) {
+        $this->collectionFactory = $collectionFactory;
+        $this->data = $data;
+        $this->urlBuilder = $urlBuilder;
+        $this->product = $product;
+    }
+
+    /**
+     * Get action options
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        if ($this->options === null) {
+            $options = $this->collectionFactory->create()->setEntityTypeFilter($this->product->getEntityType()->getEntityTypeId())->toOptionArray();
+            $this->prepareData();
+            foreach ($options as $optionCode) {
+                $this->options[$optionCode['value']] = [
+                    'type' => 'customer_group_' . $optionCode['value'],
+                    'label' => $optionCode['label'],
+                ];
+
+                if ($this->urlPath && $this->paramName) {
+                    $this->options[$optionCode['value']]['url'] = $this->urlBuilder->getUrl(
+                        $this->urlPath,
+                        [$this->paramName => $optionCode['value']]
+                    );
+                }
+
+                $this->options[$optionCode['value']] = array_merge_recursive(
+                    $this->options[$optionCode['value']],
+                    $this->additionalData
+                );
+            }
+
+            $this->options = array_values($this->options);
+        }
+
+        return $this->options;
+    }
+
+    /**
+     * Prepare addition data for subactions
+     *
+     * @return void
+     */
+    protected function prepareData()
+    {
+        foreach ($this->data as $key => $value) {
+            switch ($key) {
+                case 'urlPath':
+                    $this->urlPath = $value;
+                    break;
+                case 'paramName':
+                    $this->paramName = $value;
+                    break;
+                default:
+                    $this->additionalData[$key] = $value;
+                    break;
+            }
+        }
+    }
+}
